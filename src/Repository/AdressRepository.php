a36631ff163a578c7a5d9ae453e4a7cdf756b5cd
kacper <kacykvaa@gmail.com>
2020-11-27 15:29:23 +0100

    save work in progress

--- src/Repository/AdressRepository.php
+++ src/Repository/AdressRepository.php
@@ -18,28 +18,4 @@ class AdressRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Address::class);
     }
-
-    /**
-     * @return Address[]
-     */
-
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function findOneBySomeField($value): ?Address
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
 }
