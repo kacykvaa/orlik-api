a73ed908e0aefbe96f7c0436209121a09d29bf8a
kacper <kacykvaa@gmail.com>
2020-12-29 19:33:36 +0100

    add work in progress

--- src/Application/Repository/FacilityRepository.php
+++ src/Application/Repository/FacilityRepository.php
@@ -6,19 +6,24 @@ declare(strict_types=1);
 namespace App\Application\Repository;
 
 use App\Application\Entity\Facility;
+use App\Common\Exception\ResourceNotFoundException;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method Facility|null find($id, $lockMode = null, $lockVersion = null)
- * @method Facility|null findOneBy(array $criteria, array $orderBy = null)
- * @method Facility[]    findAll()
- * @method Facility[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
+
 class FacilityRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Facility::class);
+    }
+
+    public function getById(int $id): Facility
+    {
+        $facility  = $this->find($id);
+        if (!$facility){
+            throw new ResourceNotFoundException('Facility not found');
+        }
+        return $facility;
     }
 }
--- src/UI/Controller/GetFacilityAction.php
+++ src/UI/Controller/GetFacilityAction.php
@@ -6,6 +6,8 @@ declare(strict_types=1);
 namespace App\UI\Controller;
 
 use App\Application\Entity\Facility as FacilityEntity;
+use App\Application\Repository\FacilityRepository;
+use App\Common\Exception\ResourceNotFoundException;
 use App\UI\Model\Response\Address as AddressResponse;
 use App\UI\Model\Response\Facility as FacilityResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,26 +20,31 @@ class GetFacilityAction extends AbstractRestAction
      * @Route("/api/get/facility/{id}", name="get_facility")
      * @param int $id
      * @param SerializerInterface $serializer
+     * @param FacilityRepository $facilityRepository
      * @return Response
      */
-    public function __invoke(int $id, SerializerInterface $serializer): Response
+    public function __invoke(int $id, SerializerInterface $serializer, FacilityRepository $facilityRepository): Response
     {
-        $facility = $this->getDoctrine()
-            ->getRepository(FacilityEntity::class)
-            ->find($id);
+        try {
+            $facility = $facilityRepository->getById($id);
 
-        $address = $facility->address();
-        $responseAddress = new AddressResponse($address->id(),
-            $address->street(),
-            $address->streetNumber(),
-            $address->city(),
-            $address->postCode());
-        $responseFacility = new FacilityResponse($facility->id(),
-            $facility->name(),
-            $facility->pitchTypes(),
-            $responseAddress,
-            $facility->createdAt());
+            $address = $facility->address();
+            $responseAddress = new AddressResponse($address->id(),
+                $address->street(),
+                $address->streetNumber(),
+                $address->city(),
+                $address->postCode());
+            $responseFacility = new FacilityResponse($facility->id(),
+                $facility->name(),
+                $facility->pitchTypes(),
+                $responseAddress,
+                $facility->createdAt());
 
-        return new Response($serializer->serialize($responseFacility, 'json'));
+            return new Response($serializer->serialize($responseFacility, 'json'));
+        } catch (ResourceNotFoundException $exception) {
+            return new Response($exception->getMessage(),404);
+        }catch (\Exception $exception){
+            return new Response('Something bad happen', 500);
+        }
     }
 }
